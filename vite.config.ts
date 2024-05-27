1a950ce87578f13388f36cbb425255c7960a252f
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2024-05-27 11:00:21 +0200

    Setup config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'remoteFooter',
+      filename: 'remoteFooter.js',
+      exposes: {
+        './RemoteFooter': './src/App.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  preview: {
+    host: 'localhost',
+    port: 5174,
+    strictPort: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
